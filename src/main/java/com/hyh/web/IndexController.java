daf8a565d863d66b18627a78d7d949f92ca5ccef
hanyh <yunhaoha@usc.edu>
2020-12-22 17:47:28 +0800

    新增页面处理

--- src/main/java/com/hyh/web/IndexController.java
+++ src/main/java/com/hyh/web/IndexController.java
@@ -2,21 +2,15 @@ package com.hyh.web;
 
 import com.hyh.NotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class IndexController {
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
-//        int i = 9/0;
-//        String blog = null;
-//        if(blog == null){
-//            throw new NotFoundException("博客不存在");
-//        }
-        System.out.println("-------index-------");
+
+    @GetMapping("/")
+    public String index(Model model){
         return "index";
     }
 }
